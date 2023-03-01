7bb14051e512e5e340611c2d2824edaccdbce6ef
DinaMy03 <126537966+DinaMy03@users.noreply.github.com>
2023-03-01 23:50:48 +0800

    Add files via upload

---
+++ CGPA.java
@@ -0,0 +1,76 @@
+import java.util.Scanner;
+public class CGPA {
+    //data attributes
+    private int creditHour;
+    private int fullCreditHour;
+    private String grade;
+    private int numOfSubject;
+    private double point;
+    private double totalPoint;
+    private double gpa;
+    private int semester;
+
+    //methods of operation
+    public double calcGPA(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter number of subject: ");
+        numOfSubject = scanner.nextInt();
+        for (int i = 0; i < numOfSubject; i++){
+            System.out.print("Enter credit hour: ");
+            creditHour = scanner.nextInt();
+            System.out.print("Enter grade: ");
+            grade = scanner.next();
+            switch (grade){
+                case "A":
+                    point = creditHour * 4.00;
+                    break;
+                case "A-":
+                    point = creditHour * 3.75;
+                    break;
+                case "B+":
+                    point = creditHour * 3.50;
+                    break;
+                case "B":
+                    point = creditHour * 3.00;
+                    break;
+                case "C+":
+                    point = creditHour * 2.50;
+                    break;
+                case "C":
+                    point = creditHour * 2.00;
+                    break;
+                case "D+":
+                    point = creditHour * 1.50;
+                    break;
+                case "D":
+                    point = creditHour * 1.00;
+                    break;
+                case "F":
+                    point = creditHour * 0.00;
+                    break;
+            }
+            totalPoint += point;
+            fullCreditHour += creditHour;
+        }
+        gpa = totalPoint / fullCreditHour;
+        return gpa;
+    }
+    public void displayGPA(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter semester: ");
+        semester = scanner.nextInt();
+        System.out.println("Your GPA for semester "+ semester + " : " + String.format("%.2f", gpa));
+    }
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        CGPA semester = new CGPA();
+        ListOfCGPA fatiha = new ListOfCGPA();
+
+        semester.calcGPA();
+        semester.displayGPA();
+
+        System.out.println("CGPA: " + fatiha.calcCGPA());
+
+    }
+}
---
+++ ListOfCGPA.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class ListOfCGPA {
+    //data attributes
+    private int numOfSemester;
+    private double cgpa;
+    private double gpa;
+    private double totalGPA;
+    //methods of operations
+
+    public double calcCGPA(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter number of semester: ");
+        numOfSemester = scanner.nextInt();
+        for(int i = 0; i < numOfSemester; i++){
+            System.out.print("Enter gpa for semester " + (i+1) + " : ");
+            gpa = scanner.nextDouble();
+            totalGPA += gpa;
+        }
+        cgpa = totalGPA/numOfSemester;
+        return cgpa;
+    }
+
+}
