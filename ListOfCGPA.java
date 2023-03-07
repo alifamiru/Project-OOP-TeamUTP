3e65310acbe32900719f77ed51afecd477a8b0d1
DinaMy03 <126537966+DinaMy03@users.noreply.github.com>
2023-03-07 22:56:22 +0800

    Update ListOfCGPA.java

--- ListOfCGPA.java
+++ ListOfCGPA.java
@@ -6,19 +6,17 @@ public class ListOfCGPA {
     private double gpa;
     private double totalGPA;
     //methods of operations
-
     public double calcCGPA(){
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter number of semester: ");
         numOfSemester = scanner.nextInt();
         for(int i = 0; i < numOfSemester; i++){
-            System.out.print("Enter gpa for semester " + (i+1) + " : ");
+            System.out.print("Enter gpa for semester " + (i + 1) + " : ");
             gpa = scanner.nextDouble();
             totalGPA += gpa;
         }
         cgpa = totalGPA/numOfSemester;
-        scanner.close();
-        return cgpa;
+        return Double.parseDouble(String.format("%.2f", cgpa));
     }
 
 }
