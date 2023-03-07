ad67ee6c142572b350d45e14335498518173a27b
aliamaisara274 <126547753+aliamaisara274@users.noreply.github.com>
2023-03-07 22:11:59 +0800

    Add files via upload

--- to_do_list.java
+++ to_do_list.java
@@ -18,15 +18,15 @@ public class to_do_list {
     public void editTask(Task task,int i){
         this.list.set(i,task);
     }
-    
+
     public static void main(String[] args) {
-    	to_do_list face = new to_do_list();
-    	Task A = new Task();
-    	TaskType A_type = new TaskType();
-    	A_type.addType("Work");
-    	A.setType(A_type.getType(0));
-    	face.addTask(A);
-    	System.out.println(face.getIndex(A) +" "+ face.getTask(0).gettype());
-    	
+        to_do_list face = new to_do_list();
+        Task A = new Task();
+        TaskType A_type = new TaskType();
+        A_type.addType("Work");
+        A.setType(A_type.getType(0));
+        face.addTask(A);
+        System.out.println(face.getIndex(A) +" "+ face.getTask(0).getType());
+
     }
 }
