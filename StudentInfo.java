a8ff53246ed1e2ed4540e9b4bedd377084316306
Ahilham <126547692+Ahilham@users.noreply.github.com>
2023-03-29 21:58:55 +0800

    Add files via upload

--- StudentInfo.java
+++ StudentInfo.java
@@ -1,60 +1,33 @@
-public class StudentInfo {
+public class StudentInfo extends Name{
 
-    private Name name ; //Student Name
     private String StudentID ; //Student ID
-    private String StudentFaculty; //Student Faculty
-    private String StudentProgramme ; //Student Program
-    private String StudentYear ; // Student Year
-    private String StudentSemester ; // Student Semester
     private to_do_list ToDoList;
+    private ListOfCGPA cgpa;
 
     /*Setter method*/
-    public void setName(Name name){
-        this.name = name ;
-    } 
     public void setStudentID(String StudentID){
         this.StudentID = StudentID ;
     }
-    public void setStudentFaculty(String StudentFaculty){
-        this.StudentFaculty = StudentFaculty;
-    }
-    public void setStudentProgramme(String StudentProgramme){
-        this.StudentProgramme = StudentProgramme ;
-    }
-    public void setStudentYear(String StudentYear){
-        this.StudentYear = StudentYear ;
-    }
-    public void setStudentSemester(String StudentSemester){
-        this.StudentSemester = StudentSemester ;
-    }
+    public void setToDoList(to_do_list ToDoList) {
+		this.ToDoList = ToDoList;
+	}
+    public void setCgpa(ListOfCGPA cgpa) {
+		this.cgpa = cgpa;
+	}
 
     /* getter method */
 
-    public Name getName(){
-        return name ;
-    }
-    public String getStudentID(){
+    
+	public String getStudentID(){
         return StudentID ;
     }
-    public String getStudentFaculty(){
-        return StudentFaculty;
-    }
-    public String getStudentProgramme(){
-        return StudentProgramme ;
-    }
-    public String getStudentYear(){
-        return StudentYear ;
-    }
-    public String getStudentSemester(){
-        return StudentSemester ;
-    }
-    
     public to_do_list getToDoList() {
 		return ToDoList;
 	}
-	public void setToDoList(to_do_list ToDoList) {
-		this.ToDoList = ToDoList;
-	}
+    public ListOfCGPA getCgpa() {
+    	return cgpa;
+    }
+	
 	
 
 }
