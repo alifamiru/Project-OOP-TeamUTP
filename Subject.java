d4abf3b565f6ed8a52e82114bc680cb29a42592a
norafidah03 <126498255+norafidah03@users.noreply.github.com>
2023-03-30 01:09:10 +0800

    Update Subject.java

--- Subject.java
+++ Subject.java
@@ -1,11 +1,12 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Subject {
+public class Subject extends Lecturer{
     // ATTRIBUTES
     private String subjectName;
     private String description;
-    ArrayList<Name> lecturer = new ArrayList<>();
+    //ArrayList<Name> lecturer = new ArrayList<>();
+    private String lecturer;        // FOR UNIT TESTING ONLY
     private String venue;
     private int creditHr;
 
@@ -16,8 +17,13 @@ public class Subject {
     public void setDescription(String description){
         this.description = description;
     }
+    /*
     public void setLecturer(Name lecturer){
         this.lecturer.add(lecturer);
+    }
+     */
+    public void setLecturer(String lecturer){       // FOR UNIT TESTING ONLY
+        this.lecturer = lecturer;
     }
     public void setVenue(String venue){
         this.venue = venue;
@@ -32,7 +38,12 @@ public class Subject {
     public String getDescription(){
         return description;
     }
+    /*
     public ArrayList<Name> getLecturer(){
+        return lecturer;
+    }
+     */
+    public String getLecturer(){        // FOR UNIT TESTING ONLY
         return lecturer;
     }
     public String getVenue(){
@@ -42,9 +53,10 @@ public class Subject {
         return creditHr;
     }
 
+    /*      COMMENTING THIS METHOD TO ALLOW UNIT TESTING
     //method createSubject
     public void createSubject(){
-        System.out.println("----------Subject's Details----------");
+        System.out.println("----------------Subject's Details----------------");
 
         Scanner scanner = new Scanner(System.in);
         Lecturer lec = new Lecturer();
@@ -73,8 +85,9 @@ public class Subject {
 
         scanner.close();
 
-        System.out.println("Subject's details created successfully.");
+        System.out.println("Subject's details created successfully.\n");
     }
+     */
 
     public void displaySubject(){
         System.out.println("Subject Name: " + getSubjectName());
@@ -82,5 +95,43 @@ public class Subject {
         System.out.println("Lecturer's Name: " + getLecturer());
         System.out.println("Venue: " + getVenue());
         System.out.println("Credit Hour: " + getCreditHr());
+        System.out.println();
+    }
+
+    public static void main(String[] args){
+        // CREATING THE OBJECTS OF THE CLASS
+        Subject subject1 = new Subject();
+        Subject subject2 = new Subject();
+
+        // ENTERING SUBJECT #1 DETAILS
+        String subject1Name = "Object Oriented Programming";
+        subject1.setSubjectName(subject1Name);
+        String subject1Desc = "This course teaches about modularity and software design around data, or objects, " +
+                                "rather than functions and logic";
+        subject1.setDescription(subject1Desc);
+        String subject1Lec = "Dr Nordin Zakaria";
+        subject1.setLecturer(subject1Lec);
+        String subject1Venue = "Lecture Theater D1, Pocket D";
+        subject1.setVenue(subject1Venue);
+        int subject1CreditHr = 3;
+        subject1.setCreditHr(subject1CreditHr);
+
+        // ENTERING SUBJECT #2 DETAILS
+        String subject2Name = "Discrete Mathematics";
+        subject2.setSubjectName(subject2Name);
+        String subject2Desc = "This course enables students to strengthen and increase the understanding of discrete " +
+                                "mathematics with special emphasis on computer science applications.";
+        subject2.setDescription(subject2Desc);
+        String subject2Lec = "Sir Jale B Ahmad";
+        subject2.setLecturer(subject2Lec);
+        String subject2Venue = "Lecture Theater D3, Pocket D";
+        subject2.setVenue(subject2Venue);
+        int subject2CreditHr = 3;
+        subject2.setCreditHr(subject2CreditHr);
+
+        // DISPLAYING EACH SUBJECT DETAILS
+        subject1.displaySubject();
+        subject2.displaySubject();
+
     }
 }
