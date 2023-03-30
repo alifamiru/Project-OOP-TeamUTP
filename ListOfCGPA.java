3bfad5ba2568cdeec77a606bccb358a0379e6680
alifamiru <126650191+alifamiru@users.noreply.github.com>
2023-03-30 22:28:02 +0800

    Update ListOfCGPA.java

--- ListOfCGPA.java
+++ ListOfCGPA.java
@@ -20,9 +20,4 @@ public class ListOfCGPA {
         
         return Double.parseDouble(String.format("%.2f", cgpa));
     }
-    
-    public static void main(String[] args) {
-    	
-		}
-    }
-
+  
